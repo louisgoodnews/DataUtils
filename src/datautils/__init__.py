39181930f50171c17b9a63a84bebd9bccfef63de
Louis Goodnews <louisgoodnews95@gmail.com>
2025-09-15 16:18:53 +0200

    Updated a few things.

---
+++ src/datautils/__init__.py
@@ -0,0 +1,16 @@
+"""
+Author: Louis Goodnews
+Date: 2025-09-15
+"""
+
+from typing import Final, Literal
+
+from core.core import DataConversionUtils, DataIdentificationUtils
+
+
+__all__: Final[list[str]] = [
+    "DataConversionUtils",
+    "DataIdentificationUtils",
+]
+
+__version__: Final[Literal["0.1.0"]] = "0.1.0"
